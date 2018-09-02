4e4c1b1c139a0ffa9af40e6d81ec1cf7be46f916
Rohit Sharma <work.rohitsharma@gmail.com>
2018-09-02 14:31:00 +0530

    Added APIView and viewsets

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework import status
+from . import serializers
 
 class HelloApiView(APIView):
 
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-
         sample_list = [
             'Hey',
             'Hi',
@@ -15,3 +18,49 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'success', 'data':sample_list})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'delete'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        sample_list = [
+            'Hi',
+            'Hello',
+            'Nopes'
+        ]
+        return Response({'message':'ok', 'data':sample_list})
+
+    def create(self, request, pk=None):
+        return Response({'method':'create'})
+
+    def retrieve(self, request, pk=None):
+        return Response({'method':'retireve'})
+
+    def update(self, request, pk=None):
+        return Response({'method':'update'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'method':'partial_update'})
+
+    def destroy(self, request, pk=None):
+        return Response({'method':'destroy'})
