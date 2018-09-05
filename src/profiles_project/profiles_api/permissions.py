c55f88fb45c8fee8db9636f4e568050d3b9910fc
Rohit Sharma <work.rohitsharma@gmail.com>
2018-09-05 22:03:52 +0530

    Added Search Feature

---
+++ src/profiles_project/profiles_api/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class UserProfilePermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.id == obj.id
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,8 +3,11 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from . import serializers
 from . import models
+from . import permissions
 
 class HelloApiView(APIView):
 
@@ -70,3 +73,7 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UserProfilePermission,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
